9677c5ebcbfd10a656bc1c0a769a4786d1d110f6
dahy30n <dhy315a32@gmail.com>
2026-03-05 09:36:13 +0900

    Add function to compute the difference of two numbers
    Implement a function to calculate the difference between two numbers.

---
+++ 두 수의 차.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int solution(int num1, int num2) {
+    return num1 - num2;
+}
+
+int main(void) {
+    int a, b;
+
+
+    int result = solution(a, b);
+    printf("%d\n", result);
+
+    return 0;
+}
