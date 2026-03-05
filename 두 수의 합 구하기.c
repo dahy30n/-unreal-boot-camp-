72cd386474e1395b8f3e00c235fe536a588d160d
dahy30n <dhy315a32@gmail.com>
2026-03-05 09:59:31 +0900

    Update 두 수의 합 구하기.c

--- 두 수의 합 구하기.c
+++ 두 수의 합 구하기.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
+int solution(int num1, int num2) {
+    return num1+ num2;
+}
+
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    
+    printf(solution);
+}
